93172c725dc4a5228de6a9516cdd5e26aa910731
Nikita <nikita.kahskin@gmail.com>
2026-01-13 14:53:57 +0100

    zrobione

--- NikitaK/NikitaK.cpp
+++ NikitaK/NikitaK.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cstdlib>
 #include <stdlib.h>
 
 #include "headerFile.h";//prototypy funkcji
@@ -17,28 +18,159 @@ int main() {
         menu();
         menuProgram(tab_wartosci, tab_jednostki);
 
-		cout << "Wpisz enter aby kontynuowac..." << endl;
+		cout << "\nWpisz enter aby kontynuowac..." << endl;
         cin.ignore();
         cin.get();
         system("cls");
     }
 }
 
-void wartosc(double war[], float st_wart, float nw_wart) {
-	if (dataCounter != limit) {
-        war[dataCounter] = st_wart;
-        war[dataCounter + 1] = nw_wart;
+void wartosc(double war[], float st_wart, float nw_wart, int wsk) {
+	if (wsk < limit) {
+        war[wsk] = st_wart;
+        war[wsk + 1] = nw_wart;
     }   
     else cout << "Osiagnieto limit przechowywanych danych!" << endl;
     return;
 }
 
-void jednostka(char jed[], char st_jed, char nw_jed) {
-    if (dataCounter != limit) {
-        jed[dataCounter] = st_jed;
-        jed[dataCounter + 1] = nw_jed;
+void jednostka(char jed[], char st_jed, char nw_jed, int wsk) {
+    if (wsk < limit) {
+        jed[wsk] = st_jed;
+        jed[wsk + 1] = nw_jed;
     }
 	return;
+}
+
+void los_his(double wart[], char jedn[]) {
+    int liczba;
+	float fahr, celsiusz, kelwin;
+	fahr = celsiusz = kelwin = 0;
+    
+    cout << "Ile losowych wartosci przeliczyc: ";
+	cin >> liczba;
+
+    if (liczba > (limit - dataCounter) / 2) {
+        cout << "Czy wygenerować tylko tyle losówek ile zostało miejsca, czy nie?(1 - tak, 0 - nie): ";
+        if (cin >> liczba && liczba == 1) {
+            liczba = (limit - dataCounter) / 2;
+        }
+        else {
+            cout << "Nie można wygenerować tylu losowych wartości!" << endl;
+            return;
+        }
+    }
+    if (liczba <= (limit - dataCounter) / 2) {
+        for (int i = 0; i < liczba; i++) {
+            switch (1 + (rand() % 3))
+            {
+            case 1://F to C
+                fahr = rand() % 1459 - 459; //od -459 do 1814
+                celsiusz = FtoC(fahr);
+                wartosc(wart, fahr, celsiusz, dataCounter);
+                jednostka(jedn, 'F', 'C', dataCounter);
+                dataCounter += 2;
+                break;
+            case 2: //F to K
+                fahr = rand() % 1459 - 459; //od -459 do 1814
+                kelwin = FtoK(fahr);
+                wartosc(wart, fahr, kelwin, dataCounter);
+                jednostka(jedn, 'F', 'K', dataCounter);
+                dataCounter += 2;
+                break;
+            case 3://C to K
+                celsiusz = rand() % 1000 - 273;
+                kelwin = CtoK(celsiusz);
+                wartosc(wart, celsiusz, kelwin, dataCounter);
+                jednostka(jedn, 'C', 'K', dataCounter);
+                dataCounter += 2;
+                break;
+            case 4://C to F
+                celsiusz = rand() % 1000 - 273; //od -273 do 1000
+                fahr = CtoF(celsiusz);
+                wartosc(wart, celsiusz, fahr, dataCounter);
+                jednostka(jedn, 'C', 'F', dataCounter);
+                dataCounter += 2;
+                break;
+            case 5://K to C
+                kelwin = rand() % 1000 + 1; //od -273 do 1000
+                celsiusz = KtoC(kelwin);
+                wartosc(wart, kelwin, celsiusz, dataCounter);
+                jednostka(jedn, 'K', 'C', dataCounter);
+                dataCounter += 2;
+                break;
+            case 6://K to F
+                kelwin = rand() % 1000 + 1; //od -273 do 1000
+                fahr = KtoF(kelwin);
+                wartosc(wart, kelwin, fahr, dataCounter);
+                jednostka(jedn, 'K', 'F', dataCounter);
+                dataCounter += 2;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    system("cls");
+    wyswietl_his(wart, jedn);
+}
+
+void modyf_his(double wart[], char jedn[]) {
+    float st_wart, nw_wart;
+    char st_jed, nw_jed;
+    int modyf;
+
+    system("cls");
+    wyswietl_his(wart, jedn);
+
+    cout << "Wpisz ktory wpisch chcesz zmodyfikowac: ";
+    cin >> modyf;
+    cout << "Podaj nowa wartosc poczatkowa: ";
+    cin >> st_wart;
+    cout << "Podaj nowa jednostke poczatkowa: ";
+    cin >> st_jed;
+    cout << "Na jaka jednostke chcesz przeliczyc (F/C/K): ";
+    cin >> nw_jed;
+
+    if (st_jed == 'F') {
+        check(st_wart, st_jed);
+        if (nw_jed == 'C')
+            nw_wart = FtoC(st_wart);
+        else if (nw_jed == 'K')
+            nw_wart = FtoK(st_wart);
+        else {
+            cout << "Bledna jednostka docelowa!" << endl;
+        }
+
+    }
+    else if (st_jed == 'C') {
+        check(st_wart, st_jed);
+        if (nw_jed == 'F')
+            nw_wart = CtoF(st_wart);
+        else if (nw_jed == 'K')
+            nw_wart = CtoK(st_wart);
+        else {
+            cout << "Bledna jednostka docelowa!" << endl;
+        }
+    }
+    else if (st_jed == 'K') {
+        check(st_wart, st_jed);
+        if (nw_jed == 'C')
+            nw_wart = KtoC(st_wart);
+        else if (nw_jed == 'F')
+            nw_wart = KtoF(st_wart);
+        else {
+            cout << "Bledna jednostka docelowa!" << endl;
+        }
+    }
+    if (st_jed != nw_jed) {
+        wartosc(wart, st_wart, nw_wart, (modyf - 1) * 2);
+        jednostka(jedn, st_jed, nw_jed, (modyf - 1) * 2);
+
+        system("cls");
+        wyswietl_his(wart, jedn);
+    }
+
 }
 
 void usun_his(double wartosc[], char jednostka[]) {
@@ -68,7 +200,7 @@ void usun_his(double wartosc[], char jednostka[]) {
 
 int wybor_his() {
 	int wybor;
-    cout << "1.Tylko C - >\n2.Tylko F - >\n3.Tylko K - >\n";
+    cout << "\n1.Tylko C - >\n2.Tylko F - >\n3.Tylko K - >\n"<<endl;
 	cin >> wybor;
     if (wybor >= 1 && wybor <= 3)
         return wybor;
@@ -90,6 +222,7 @@ void wyswietl_his(double wartosc[], char jednostka[]) {
 
 void wyswietl_his_new(double wartosc[], char jednostka[], char jed) {
     int num = 1;
+    cout << "\n";
     for (int i = 0; i < dataCounter; i += 2) {
         if (jednostka[i] == jed) {
             cout << "<" << num << ">" << " " << wartosc[i] << jednostka[i] << " = " << wartosc[i + 1] << jednostka[i + 1] << endl;
@@ -98,6 +231,7 @@ void wyswietl_his_new(double wartosc[], char jednostka[], char jed) {
     }
     if (num == 1)
         cout << "Brak danych do wyswietlenia!" << endl;
+    cout << "\n";
 }
 
 void his(double wartosc[], char jednostka[], int wybor) {
@@ -125,47 +259,38 @@ void his(double wartosc[], char jednostka[], int wybor) {
 int check(float temp, char jedn) {
     if (jedn == 'F' && temp < -459.67) {
         cout << "Bledna wartosc temperatury dla stopni Fahrenheita!" << endl;
-        return 0;
+        return -999;
     }
     else if (jedn == 'C' && temp < -273.15) {
         cout << "Bledna wartosc temperatury dla stopni Celsjusza!" << endl;
-        return 0;
+        return -999;
     }
     else if (jedn == 'K' && temp < 0) {
         cout << "Bledna wartosc temperatury dla stopni Kelwina!" << endl;
-        return 0;
+        return -999;
     }
     else {
-        return 1;
+        return temp;
     }
 }
 
 float pobiezF(float stopnie) {
     cout << "Podaj stopnie Fahr: ";
     cin >> stopnie;
-	int che = check(stopnie, 'F');
-    if (che == 1) {
-        return stopnie;
-	}
-    else { return -999; }
+	stopnie = check(stopnie, 'F');
+	return stopnie;
 }
 float pobiezC(float stopnie) {
     cout << "Podaj stopnie Celsius: ";
     cin >> stopnie;
-	int che = check(stopnie, 'C');
-    if (che == 1) {
-        return stopnie;
-    }
-    else { return -999; }
+    stopnie = check(stopnie, 'C');
+    return stopnie;
 }
 float pobiezK(float stopnie) {
     cout << "Podaj stopnie Kelwin: ";
     cin >> stopnie;
-	int che = check(stopnie, 'K');
-    if (che == 1) {
-        return stopnie;
-    }
-    else { return -999; }
+    stopnie = check(stopnie, 'K');
+    return stopnie;
 }
 
 void menu() {
@@ -174,11 +299,13 @@ void menu() {
     cout << "3 - przelicz Celsius -> Fahr" << endl;
     cout << "4 - przelicz Celsius -> Kelwin" << endl;
     cout << "5 - przelicz Kelwin -> Celsius" << endl;
-    cout << "6 - przelicz Kelwin -> Fahr" << endl;
+    cout << "6 - przelicz Kelwin -> Fahr\n" << endl;
 	cout << "7 - pokaz historie" << endl;
     cout << "8 - usun z historii" << endl;
-    cout << "9 - zakoncz dzialanie programu" << endl;
-    cout << "Wybierz funkcje konwersji temperatury: " << endl;
+    cout << "9 - modyfikuj historie" << endl;
+    cout << "10 - losowe wypelnienie historii" << endl;
+    cout << "11 - zakoncz dzialanie programu\n" << endl;
+    cout << "Wybierz funkcje konwersji temperatury: ";
 }
 
 void menuProgram (double tab_wartosci[], char tab_jednostki[]) {
@@ -191,10 +318,10 @@ void menuProgram (double tab_wartosci[], char tab_jednostki[]) {
     case 1:
         fahr = pobiezF(0);
         celsius = FtoC(fahr);
-        cout << fahr << "   " << celsius << endl;
-        wartosc(tab_wartosci, fahr, celsius);
-        jednostka(tab_jednostki, 'F', 'C');
-        dataCounter += 2;
+        cout << "\n" << fahr << "   - >   " << celsius << "\n" << endl;
+        wartosc(tab_wartosci, fahr, celsius, dataCounter);
+        jednostka(tab_jednostki, 'F', 'C', dataCounter);
+        if (dataCounter < limit) dataCounter += 2;
         /*
         tab_wartosci[dataCounter] = fahr;
         tab_jednostki[dataCounter] = 'F';
@@ -206,48 +333,54 @@ void menuProgram (double tab_wartosci[], char tab_jednostki[]) {
     case 2:
         fahr = pobiezF(0);
         kelwin = FtoK(fahr);
-        cout << fahr << "   " << kelwin << endl;
-        wartosc(tab_wartosci, fahr, kelwin);
-        jednostka(tab_jednostki, 'F', 'K');
-        dataCounter += 2;
+        cout << "\n" << fahr << "   ->   " << kelwin << "\n" << endl;
+        wartosc(tab_wartosci, fahr, kelwin, dataCounter);
+        jednostka(tab_jednostki, 'F', 'K', dataCounter);
+        if (dataCounter < limit) dataCounter += 2;
         break;
     case 3:
         celsius = pobiezC(0);
         fahr = CtoF(celsius);
-        cout << celsius << "   " << fahr << endl;
-        wartosc(tab_wartosci, celsius, fahr);
-        jednostka(tab_jednostki, 'C', 'F');
-        dataCounter += 2;
+        cout << "\n" << celsius << "   ->   " << fahr << "\n" << endl;
+        wartosc(tab_wartosci, celsius, fahr, dataCounter);
+        jednostka(tab_jednostki, 'C', 'F', dataCounter);
+        if (dataCounter < limit) dataCounter += 2;
         break;
     case 4:
         celsius = pobiezC(0);
         kelwin = CtoK(celsius);
-        cout << celsius << "   " << kelwin << endl;
-        wartosc(tab_wartosci, celsius, kelwin);
-        jednostka(tab_jednostki, 'C', 'K');
-        dataCounter += 2;
+        cout <<"\n" << celsius << "   ->   " << kelwin << "\n" << endl;
+        wartosc(tab_wartosci, celsius, kelwin, dataCounter);
+        jednostka(tab_jednostki, 'C', 'K', dataCounter);
+        if (dataCounter < limit) dataCounter += 2;
         break;
     case 5:
         kelwin = pobiezK(0);
         celsius = KtoC(kelwin);
-        cout << kelwin << "   " << celsius << endl;
-        wartosc(tab_wartosci, kelwin, celsius);
-        jednostka(tab_jednostki, 'K', 'C');
-        dataCounter += 2;
+        cout << "\n" << kelwin << "   ->   " << celsius << "\n" << endl;
+        wartosc(tab_wartosci, kelwin, celsius, dataCounter);
+        jednostka(tab_jednostki, 'K', 'C', dataCounter);
+        if (dataCounter < limit) dataCounter += 2;
         break;
     case 6:
         kelwin = pobiezK(0);
         fahr = KtoF(kelwin);
-        cout << kelwin << "   " << fahr << endl;
-        wartosc(tab_wartosci, kelwin, fahr);
-        jednostka(tab_jednostki, 'K', 'F');
-        dataCounter += 2;
+        cout << "\n" << kelwin << "   ->   " << fahr << "\n" << endl;
+        wartosc(tab_wartosci, kelwin, fahr, dataCounter);
+        jednostka(tab_jednostki, 'K', 'F', dataCounter);
+        if (dataCounter < limit) dataCounter += 2;
         break;
     case 7:
         his(tab_wartosci, tab_jednostki, wybor_his());
         break;
     case 8:
-		usun_his(tab_wartosci, tab_jednostki);
+		usun_his(tab_wartosci, tab_jednostki);  
+        break;
+    case 9:
+        modyf_his(tab_wartosci, tab_jednostki);
+		break;
+    case 10: 
+		los_his(tab_wartosci, tab_jednostki);
         break;
     default: exit(0);
     }
--- NikitaK/headerFile.h
+++ NikitaK/headerFile.h
@@ -15,11 +15,13 @@ float KtoF(float stopnie);
 void menu();
 void menuProgram(double tab_wartosci[], char tab_jednostki[]);
 
-void wartosc(double war[], float st_wart, float nw_wart);
-void jednostka(char jed[], char st_jed, char nw_jed);
+void wartosc(double war[], float st_wart, float nw_wart, int wsk);
+void jednostka(char jed[], char st_jed, char nw_jed, int wsk);
 
-void his(double wartosc[], char jednostka[], int wybor);//his
+void his(double wartosc[], char jednostka[], int wybor);
 void wyswietl_his(double wartosc[], char jednostka[]);
 void wyswietl_his_new(double wartosc[], char jednostka[], char jed);
-int wybor_his();//hys_wybor
-void usun_his(double wartosc[], char jednostka[]);//hisUs
+int wybor_his();
+void usun_his(double wartosc[], char jednostka[]);
+void modyf_his(double wartosc[], char jednostka[]);
+void los_his(double wartosc[], char jednostka[]);
